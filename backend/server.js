70daaad342362678f0d2844fbcc3cfde7f060abe
Vaishali-Sahu-04 <vaishusahuone@gmail.com>
2024-05-22 10:07:47 +0530

    ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ import connectMongoDb from './db/connectMongoDb.js'
 dotenv.config();
 const app = express()
 const PORT=process.env.PORT || 5000;
-//const __dirname=path.resolve();
+const __dirname=path.resolve();
 
 
 app.use(cors({
@@ -20,11 +20,11 @@ app.use(cors({
 
 app.use("/api/data",dataRoutes);
 
-// app.use(express.static(path.join(__dirname,"/client/build")));
+app.use(express.static(path.join(__dirname,"/frontendd/build")));
 
-// app.get("*",(req,res)=> {
-//     res.sendFile(path.join(__dirname,"client","build","index.html"));
-// })
+app.get("*",(req,res)=> {
+    res.sendFile(path.join(__dirname,"frontendd","build","index.html"));
+})
 
 app.listen(PORT,()=>{
     console.log("Server started on port",PORT)
